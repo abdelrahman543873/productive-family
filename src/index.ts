49b7ebd47e6a5974de635051430dcd4a89364d80
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-02 15:59:12 +0200

    swagger init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Server } from 'http';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as awsServerlessExpress from 'aws-serverless-express';
 import * as express from 'express';
@@ -12,15 +13,23 @@ const bootstrapServer = async (): Promise<Server> => {
   const expressApp = express();
   const adapter = new ExpressAdapter(expressApp);
   const app = await NestFactory.create(AppModule, adapter);
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.init();
   return awsServerlessExpress.createServer(expressApp);
-}
+};
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
   if (!cachedServer) {
-    cachedServer = await bootstrapServer()
+    cachedServer = await bootstrapServer();
   }
   return awsServerlessExpress.proxy(cachedServer, event, context, 'PROMISE')
-      .promise;
+    .promise;
 };
