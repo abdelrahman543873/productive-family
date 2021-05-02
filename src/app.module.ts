900ca081895d55c6c3aa9f0b6bfdcff8f10a6a67
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-02 16:44:16 +0200

    added database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FaqModule } from './faq/faq.module';
 
 @Module({
-  imports: [FaqModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          uri:
+            'mongodb://bodi:Vindiesel33$@cluster0-shard-00-00.dd7up.mongodb.net:27017,cluster0-shard-00-01.dd7up.mongodb.net:27017,cluster0-shard-00-02.dd7up.mongodb.net:27017/test?replicaSet=atlas-nls3wr-shard-0&ssl=true&authSource=admin',
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+        };
+      },
+    }),
+    FaqModule,
+  ],
 })
 export class AppModule {}
