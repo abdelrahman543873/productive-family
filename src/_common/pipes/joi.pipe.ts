85c4cab4ffef5677387bfb904eea010782876646
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-15 14:28:17 +0200

    pipe refactor

--- src/_common/pipes/joi.pipe.ts
+++ src/_common/pipes/joi.pipe.ts
@@ -1,19 +1,11 @@
-import {
-  PipeTransform,
-  Injectable,
-  ArgumentMetadata,
-  BadRequestException,
-} from '@nestjs/common';
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 import { ObjectSchema } from 'joi';
 
 @Injectable()
 export class JoiValidationPipe implements PipeTransform {
   constructor(private schema: ObjectSchema) {}
 
-  async transform(
-    value: Record<any, any>,
-    metadata: ArgumentMetadata,
-  ): Promise<Record<any, any>> {
+  async transform(value: Record<any, any>): Promise<Record<any, any>> {
     const { error } = this.schema.validate(value);
     if (error) {
       throw new BadRequestException(error.message);
