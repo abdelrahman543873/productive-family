c59c0e6db4b83e761bd5e95ca127fded9dd6b493
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-20 13:17:37 +0200

    refactor: get driver orders test refactor

--- test/orders/get-driver-orders.spec.ts
+++ test/orders/get-driver-orders.spec.ts
@@ -27,17 +27,24 @@ describe('get driver orders suite case', () => {
     const driver = await driverFactory();
     const provider = await providerFactory();
     await ordersFactory(10, { driver: driver._id, provider: provider._id });
+    // another record to make sure it's only getting orders for this provider
+    await orderFactory({ driver: driver._id });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: `${GET_DRIVER_ORDERS}?provider=${provider._id}`,
       token: driver.token,
     });
+    expect(res.body.data.docs[0].provider).toBe(
+      decodeURI(encodeURI(`${provider._id}`)),
+    );
     expect(res.body.data.docs.length).toBe(10);
   });
 
   it('should get driver active orders ', async () => {
     const driver = await driverFactory();
     await ordersFactory(10, { driver: driver._id, state: OrderEnum.SHIPPING });
+    // another record to make sure it's only getting orders for this state
+    await orderFactory({ driver: driver._id, state: OrderEnum.PREPARING });
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.GET,
       url: `${GET_DRIVER_ORDERS}?state=${OrderEnum.SHIPPING}`,
