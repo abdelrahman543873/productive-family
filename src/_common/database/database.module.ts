2a536d5b32ad04030bf7aae1bf9f7aef0419b91a
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-23 17:22:08 +0200

    .env hotfix

--- src/_common/database/database.module.ts
+++ src/_common/database/database.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
-    MongooseModule.forRoot(env.MONGO_DB ?? env.LOCAL_DB, {
+    MongooseModule.forRoot(env.MONGO_DB, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
