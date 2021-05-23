528780b292fcd18afd7c15082fd8054b60f13141
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-23 15:53:50 +0200

    fix: mongodb connection

--- src/_common/database/database.module.ts
+++ src/_common/database/database.module.ts
@@ -4,12 +4,15 @@ import { env } from '../utils/env';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(env.MONGO_DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }),
+    MongooseModule.forRoot(
+      env.RUN_INSIDE_DOCKER ? env.MONGO_DB : env.LOCAL_MONGO_DB,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
   ],
 })
 export class DataBaseModule {}
