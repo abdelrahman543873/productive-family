b72daf06ba8f0b0a1f4a4f15293959e5b336da4b
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-05-27 09:56:26 +0200

    before test run refactoring

--- test/before-test-run.ts
+++ test/before-test-run.ts
@@ -1,18 +1,39 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { HttpExceptionFilter } from 'src/_common/exceptions/exception.filter';
 import { TransformInterceptor } from 'src/_common/interceptors/response.interceptor';
 import { Test, TestingModule } from '@nestjs/testing';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import * as multer from 'fastify-multer';
 import { MongoExceptionFilter } from 'src/_common/exceptions/mongo-exception.filter';
 import { ValidationMongooseError } from 'src/_common/exceptions/validation-excpetion-filter';
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
 
 // this is done this way to be able to inject repositories into factories
 export const moduleRef = async (): Promise<TestingModule> => {
   return await Test.createTestingModule({
     imports: [AppModule],
+    providers: [
+      {
+        provide: APP_INTERCEPTOR,
+        useClass: TransformInterceptor,
+      },
+      {
+        provide: APP_FILTER,
+        useClass: HttpExceptionFilter,
+      },
+      {
+        provide: APP_FILTER,
+        useClass: MongoExceptionFilter,
+      },
+      {
+        provide: APP_FILTER,
+        useClass: ValidationMongooseError,
+      },
+    ],
   }).compile();
 };
 // to be imported for making requests
@@ -23,14 +44,8 @@ beforeAll(async () => {
   const module = await moduleRef();
   const fastifyAdapter = new FastifyAdapter();
   fastifyAdapter.register(multer.contentParser);
-  app = module.createNestApplication(fastifyAdapter);
-  app.useGlobalInterceptors(new TransformInterceptor());
+  app = module.createNestApplication<NestFastifyApplication>(fastifyAdapter);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalFilters(
-    new HttpExceptionFilter(),
-    new MongoExceptionFilter(),
-    new ValidationMongooseError(),
-  );
   await app.init();
   await app
     .getHttpAdapter()
@@ -38,6 +53,6 @@ beforeAll(async () => {
     .ready();
 });
 
-afterAll(async done => {
-  done();
+afterAll(async () => {
+  await app.close();
 });
